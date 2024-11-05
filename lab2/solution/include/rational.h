59bdbb63df5ee0874209a5fcc134b494d60c124b
Alessio Modonesi <alessiomodo2004@gmail.com>
2024-11-05 17:19:25 +0100

    caricate soluzioni lab2

---
+++ lab2/solution/include/rational.h
@@ -0,0 +1,105 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#include <ostream>
+
+// Invarianti:
+// - denominator_ è sempre diverso da 0
+// - la frazione è sempre in forma normale
+// NOTA: il secondo invariante mi costringe a normalizzare la frazione
+//       per ogni set di numeratore e denominatore (oltre che nel costruttore)
+class Rational
+{
+public:
+    // Un solo costruttore che gestisce tre casi:
+    // - nessun argomento (frazione inizializzata a 0)
+    // - un solo argomento (numero intero perché il denominatore è 1)
+    // - due argomenti
+    // NOTA: questo è possibile sfruttando gli argomenti di default, che
+    //       fanno riferimento a due costanti definite in fondo alla classe.
+    Rational(int num = kDefaultNumerator, int den = kDefaultDenominator);
+    
+    // Assignment - gli altri operatori sono implementati come helper function
+    // NOTA: l'argomento è passato per copia e non per const reference
+    // perché copio solo due int (ed è probabilmente più veloce che sfruttare una reference)
+    Rational& operator=(Rational a);
+
+    // accessors / get functions: stesso nome della variabile
+    // mutators / set functions: set_<var name>
+    // Approfondimento:
+    // https://google.github.io/styleguide/cppguide.html#Function_Names
+    // NOTA: definite in-class, sono inline (ragionevole per queste funzioni con una sola istruzione)
+    int numerator(void) const { return numerator_; }
+    int denominator(void) const { return denominator_; }
+    
+    // Implementano l'invariante secondo cui la frazione è sempre in forma normale
+    // (quindi dividono numeratore e denominatore per Gcd
+    void set_numerator(int num);
+    void set_denominator(int den);
+    
+    // NOTA: devo fare il cast di numerator_, altrimenti eseguirebbe prima la divisione
+    // intera e poi la conversione a double. Convertire il denominatore non serve perché,
+    // essendo il primo operando double, il compilatore è costretto a convertire anche il secondo
+    // e ad eseguire la divisione tra double.
+    // NOTA: implementato in-class, quindi funzione inline - è un cast + una divisione, è ragionevole.
+    double ToDouble(void) const { return static_cast<double> (numerator_) / denominator_; }
+    
+    // Fornisco anche l'interfaccia tramite cast
+    // NOTA: all'epoca del laboratorio non sapevate ancora che si potesse fare.
+    //       Per questo ho previsto sia la funzione ToDouble che l'overloading del cast,
+    //       ma normalmente avrei previsto solo il cast (senza implementare anche ToDouble).
+    operator double() const { return ToDouble(); }
+    
+    // Per la gestione delle eccezioni. Definita qui in fondo perché ho preferito mettere
+    // in evidenza i costruttori e l'interfaccia che si usa in condizioni normali (mentre questa classe
+    // si usa in condizioni di errore).
+    class DivideByZeroException {};
+    
+private:
+    // Funzione membro privata, usata solo dalle altre funzioni membro
+    void normalize(void);
+    
+    // Notare l'uso di _ alla fine, seguendo la convenzione di Google
+    int numerator_;
+    int denominator_;
+    
+    // Default initializers
+    // Preferisco definire qui le costanti e usarle nella dichiarazione
+    // del costruttore come valori di default
+    // Sono static per averne una sola istanza, comune a tutti gli oggetti di questa classe
+    static constexpr int kDefaultNumerator = 0;
+    static constexpr int kDefaultDenominator = 1;
+};
+
+// Nota:
+// https://google.github.io/styleguide/cppguide.html#Classes
+// Prefer to define non-modifying binary operators as non-member functions.
+// If a binary operator is defined as a class member,
+// implicit conversions will apply to the right-hand argument,
+// but not the left-hand one.
+// It will confuse your users if a < b compiles but b < a doesn't.
+
+// Addition
+Rational operator+(Rational a, Rational b);
+// Subtraction
+Rational operator-(Rational a, Rational b);
+// Multiplication
+Rational operator*(Rational a, Rational b);
+// Division
+Rational operator/(Rational a, Rational b);
+// Equality
+bool operator==(Rational a, Rational b);
+// Output to stream - questo non era richiesto dall'esercizio, ma mi è comodo per il debug
+std::ostream& operator<<(std::ostream& os, Rational a);
+
+// Le funzioni seguenti sono slegate dalla classe e disponibili all'utente.
+// Questo non costituisce nessun problema. Eventualmente potrebbero essere spostate in un modulo
+// separato (una mini libreria), usata anche da Rational.
+
+// Greatest common divisor
+int Gcd(int a, int b);
+// Least common multiple
+int Lcm(int a, int b);
+
+
+#endif // RATIONAL_H
---
+++ lab2/solution/src/rational.cpp
@@ -0,0 +1,141 @@
+#include "rational.h"
+
+Rational::Rational(int num, int den)
+    : numerator_{num}, denominator_{den}
+{
+    if (den == 0)
+        throw DivideByZeroException();
+    // Invariante di forma normale
+    normalize();
+}
+
+// Questa funzione potrebbe essere anche definita in-class, ma set_denominator() è troppo lunga per essere
+// in-class a sua volta (vedi implementazione qui sotto). Quindi ho raggruppato entrambe le funzioni qui.
+void Rational::set_numerator(int num)
+{
+    numerator_ = num;
+    normalize();
+    
+    return;
+}
+
+void Rational::set_denominator(int den)
+{
+    if (den == 0)
+        throw DivideByZeroException();
+    denominator_ = den;
+    normalize();
+    
+    return;
+}
+
+// NOTA: questa funzione sposta il negativo al numeratore (se num e den sono discordi).
+// Quindi non è necessario gestire il caso di numeratore positivo
+// e denominatore negativo in operator<<.
+// Se num e den sono negativi li rende positivi.
+void Rational::normalize(void)
+{
+    int gcd = Gcd(numerator_, denominator_);
+    if (gcd != 1)
+    {
+        numerator_ /= gcd;
+        denominator_ /= gcd;
+    }
+    
+    return;
+}
+
+// Assignment
+Rational& Rational::operator=(Rational a)
+{
+    // NOTA: essendo una funzione membro, accedo direttamente ai membri privati
+    // NOTA: copio senza verifiche perché a rispetta gli invarianti
+    numerator_ = a.numerator_;
+    denominator_ = a.denominator_;
+    
+    return *this;
+}
+
+// Addition
+Rational operator+(Rational a, Rational b)
+{
+    // Notate come una buona scelta dei nomi delle variabili membro e quindi degli accessor
+    // renda il codice molto leggibile
+    int common_den = Lcm(a.denominator(), b.denominator());
+    int num_a_common = a.numerator() * (common_den / a.denominator());
+    int num_b_common = b.numerator() * (common_den / b.denominator());
+    
+    // Sfrutto l'invariante: la frazione sarà normalizzata nel costruttore
+    // Stesso ragionamento negli operator seguenti
+    return Rational(num_a_common + num_b_common, common_den);
+}
+
+// Subtraction
+Rational operator-(Rational a, Rational b)
+{
+    // Il codice è quasi identico a operator+, però non ho implementato una funzione
+    // per queste tre righe di codice perché:
+    int common_den = Lcm(a.denominator(), b.denominator());
+    int num_a_common = a.numerator() * (common_den / a.denominator());
+    int num_b_common = b.numerator() * (common_den / b.denominator());
+    
+    return Rational(num_a_common - num_b_common, common_den);
+}
+
+// Multiplication
+Rational operator*(Rational a, Rational b)
+{
+    // Una versione maggiormente robusta agli overflow avrebbe potuto valutare
+    // eventuali fattori comuni tra:
+    // a.numerator() e b.denominator()
+    // a.denominator() e b.numerator()
+    return Rational (a.numerator() * b.numerator(), a.denominator() * b.denominator());
+}
+
+// Division
+Rational operator/(Rational a, Rational b)
+{
+    // Avrei potuto creare una funzione che calcola il reciproco, ma nel contesto
+    // dell'esercizio tale funzione sarebbe servita solo qui, quindi non l'ho fatto
+    // NOTA: l'eventuale divisione per zero è gestita nel costruttore chiamato
+    // nel return statement di operator*.
+    return a * Rational(b.denominator(), b.numerator());
+}
+
+// Equality
+bool operator==(Rational a, Rational b)
+{
+    // NOTA: l'invariante di forma normale mi permette di considerare solo il caso in cui
+    // numeratore e denominatore sono uguali
+    return a.numerator() == b.numerator() && a.denominator() == b.denominator();
+}
+
+// Output to stream - questo non era richiesto dall'esercizio, ma mi è comodo per il debug
+std::ostream& operator<<(std::ostream& os, Rational a)
+{
+    return os << a.numerator() << "/" << a.denominator();
+}
+
+
+// Greatest common divisor
+// Algoritmo euclideo + modulo operator
+// Approfondimenti:
+// - https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+// - https://www.geeksforgeeks.org/c-program-find-gcd-hcf-two-numbers/
+// NOTA: il numero di passi dell'algoritmo è basso per qualunque intero rappresentabile,
+// perciò non ci sono problemi di stack overflow
+int Gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+        
+    return Gcd(b, a % b);
+}
+
+// Least common multiple
+// Vedi riferimenti per Gcd
+int Lcm(int a, int b)
+{
+    // NOTE: dividing before multiplying avoids overflow
+    return a / Gcd(a, b) * b;
+}
